ab46bfd2c38fdf8f3ddf066f1c36ab0432208563
ahlyel-amine <>
2024-08-04 18:41:46 +0100

    [init] load_config

--- include/config.h
+++ include/config.h
@@ -1,7 +1,20 @@
 #ifndef CONFIG_H
 # define CONFIG_H
 # include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 
-bool	load_config(char *conf);
+#define MAX_CONFIG_LINE_LENGTH 256
+#define MAX_CONFIG_KEY_LENGTH 50
+#define MAX_CONFIG_VALUE_LENGTH 200
+
+typedef struct s_config{
+    char key[MAX_CONFIG_KEY_LENGTH];
+    char value[MAX_CONFIG_VALUE_LENGTH];
+} t_config;
+
+static t_config config_entries[100];
+static int config_entry_count = 0;
 
 #endif
--- src/config/load_config.c
+++ src/config/load_config.c
@@ -1,7 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "config.h"
 
-bool    load_config(char *conf)
+int load_config(const char *filepath)
 {
-    (void)conf;
-    return (true);
+    FILE *file = fopen(filepath, "r");
+    if (!file) {
+        perror("Failed to open config file");
+        return -1;
+    }
+
+    char line[MAX_CONFIG_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+        char *key = strtok(line, "=");
+        char *value = strtok(NULL, "\n");
+
+        if (key && value) {
+            strncpy(config_entries[config_entry_count].key, key, MAX_CONFIG_KEY_LENGTH);
+            strncpy(config_entries[config_entry_count].value, value, MAX_CONFIG_VALUE_LENGTH);
+            config_entry_count++;
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
+
+const char* get_config_value(const char *key)
+{
+    for (int i = 0; i < config_entry_count; i++) {
+        if (strcmp(config_entries[i].key, key) == 0) {
+            return config_entries[i].value;
+        }
+    }
+    return NULL;
 }
